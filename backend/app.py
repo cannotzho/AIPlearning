1d1b40171705fded487d57aa5224f7aa136f57ca
cannotzho <62131975+cannotzho@users.noreply.github.com>
2025-10-05 18:27:56 +0800

    Create app.py
    Server file with some endpoint functionality

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, redirect
+from sqlalchemy_utils import database_exists
+from src.routes import api_bp
+from src.models import db, KeywordModel, VideoModel
+import os
+
+UPLOADS_FOLDER = 'uploads'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///processed_videos.db'
+
+db.init_app(app)
+app.register_blueprint(api_bp, url_prefix='/api')
+
+if not database_exists('sqlite:///instance/processed_videos.db'):
+    with app.app_context():
+        db.create_all()
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/reset_all', methods = ['POST', 'GET'])
+def reset_all():
+    #delete all video entries and video files in uploads folder
+    #Route currently used for testing only
+    db.session.query(VideoModel).delete()
+    db.session.commit()
+    try:
+        for filename in os.listdir(UPLOADS_FOLDER):
+            file_path = os.path.join(UPLOADS_FOLDER, filename)
+            if os.path.isfile(file_path):  # Check if it's a file, not a subdirectory
+                os.remove(file_path)
+        print(f"All files in '{UPLOADS_FOLDER}' deleted successfully.")
+    except OSError as e:
+        print(f"Error deleting files in '{UPLOADS_FOLDER}': {e}")
+    return redirect('/api/videos/')
+
+if __name__ == "__main__":
+    app.run(debug = True)
