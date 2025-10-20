101134d26a7e6c8c2767fc45c72e9f2845e5ab25
cannotzho <62131975+cannotzho@users.noreply.github.com>
2025-10-20 18:01:09 +0800

    Add upload button disabled mode
    Prevent POST request without uploading files first

--- frontend/react-app/src/components/UploadBtn.tsx
+++ frontend/react-app/src/components/UploadBtn.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 interface Props {
   upload_route: string;
 }
 
 const UploadBtn = ({ upload_route }: Props) => {
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const handleUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  };
+
   return (
     <>
       <form action={upload_route} method="POST" encType="multipart/form-data">
@@ -14,9 +24,14 @@ const UploadBtn = ({ upload_route }: Props) => {
             className="form-control"
             id="videoInput"
             name="video"
+            onChange={handleUpload}
           />
           <label className="input-group-text" htmlFor="videoInput">
-            <button type="submit" className="btn btn-primary">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={buttonDisabled}
+            >
               Upload
             </button>
           </label>
